dfe90e0dab97d5be08481e0c929f4a17cd52c1d2
lostsquirrel <songli1988@gmail.com>
2018-07-22 17:01:51 +0800

    add part of collinear

---
+++ collinear/src/main/java/BruteCollinearPoints.java
@@ -0,0 +1,30 @@
+/*************************************************************************
+ *  Compilation:  javac BruteCollinearPoints.java
+ *  Execution:    none
+ *  Dependencies: Point.java
+ *  Name:    Song Li
+ *  NetID:   lostsquirreli@hotmail.com
+ *  Precept: P03
+
+ *
+ *************************************************************************/
+
+public class BruteCollinearPoints {
+    /**
+     * // finds all line segments containing 4 points
+     * @param points 4 points
+     */
+    public BruteCollinearPoints(Point[] points) {
+
+    }
+
+    /**
+     * @return the number of line segments
+     */
+    public int numberOfSegments() {
+
+    }
+    public LineSegment[] segments() {
+
+    }
+}
