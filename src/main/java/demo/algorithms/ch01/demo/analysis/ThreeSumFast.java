5aeb38376bbca9ccc6200c7ce76ae719849bc38e
lostsquirrel <songli1988@gmail.com>
2018-08-31 07:33:42 +0800

    add another part of analysis

---
+++ src/main/java/demo/algorithms/ch01/demo/analysis/ThreeSumFast.java
@@ -0,0 +1,25 @@
+package demo.algorithms.ch01.demo.analysis;
+
+import edu.princeton.cs.algs4.BinarySearch;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Arrays;
+
+public class ThreeSumFast {
+    public static int count(int[] a) { // Count triples that sum to 0.
+        Arrays.sort(a);
+        int N = a.length;
+        int cnt = 0;
+        for (int i = 0; i < N; i++)
+            for (int j = i + 1; j < N; j++)
+                if (BinarySearch.indexOf(a, -a[i] - a[j]) > j)
+                    cnt++;
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        int[] a = new In(args[0]).readAllInts();
+        StdOut.println(count(a));
+    }
+}
---
+++ src/test/java/demo/algorithms/test/NumberTest.java
@@ -0,0 +1,13 @@
+package demo.algorithms.test;
+
+import org.junit.Test;
+
+public class NumberTest {
+    @Test
+    public void testIntegerOverflow() {
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MIN_VALUE - 1);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MAX_VALUE + 1);
+    }
+}
